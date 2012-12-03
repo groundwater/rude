c43969d0cc442f2159914ba2f0a65c1aa11f556d
Jacob Groundwater <groundwater@gmail.com>
2012-12-02 18:20:56 -0800

    Support push and pull Operations

--- bin/rude.js
+++ bin/rude.js
@@ -7,12 +7,15 @@ var Path    = require('path')
 var util    = require('util')
 
 var program = require('commander')
-var cradle  = require('cradle');
+var nano    = require('nano');
 
 var rude    = require('../index')
 
 function connect(host,port,database){
-	return new(cradle.Connection)(host,port).database(database)
+	var url = util.format('http://%s:%s/%s',host,port,database)
+	console.log(url)
+	var db  = nano(url)
+	return db
 }
 
 function Error(){
@@ -28,10 +31,10 @@ function strip(path){
 }
 
 program.version('0.0.0');
-program.option('-n, --name  <NAME>' , 'use database NAME'    ,'rude')
-program.option('-H, --host  <HOST>' , 'connect to host NAME' ,'http://localhost')
-program.option('-p, --port  <PORT>' , 'connect to port PORT' ,5984)
-program.option('-f, --file  <FILE>' , 'track assets in file FILE' ,'assets.json')
+program.option('-n, --name      <NAME>' , 'use database NAME'    ,'rude')
+program.option('-H, --host      <HOST>' , 'connect to host NAME' ,'localhost')
+program.option('-p, --port      <PORT>' , 'connect to port PORT' ,5984)
+program.option('-m, --manifest  <FILE>' , 'track assets in manifest FILE' ,'assets.json')
 
 program
 .command('init')
@@ -41,16 +44,16 @@ program
 	
 	var file
 	try{
-		file = fs.readFileSync(program.file)
+		file = fs.readFileSync(program.manifest)
 	}catch(e){
 		file = "{}"
 	}
 	
 	try{
 		var json = JSON.parse(file)
-		fs.writeFileSync(program.file,JSON.stringify(json))
+		fs.writeFileSync(program.manifest,JSON.stringify(json))
 		console.log('[OKAY] New Rude database initialized')
-		console.log('[OKAY] Asset Manifest at %s',program.file)
+		console.log('[OKAY] Asset Manifest at %s',program.manifest)
 	}catch(e){
 		console.error('[FAIL] Asset File Exists and is Invalid')
 	}
@@ -58,9 +61,46 @@ program
 	
 })
 
+function insert(db, doc,id,name,data,json,hash,file){
+	db.insert(doc, id, function(err,res){
+		
+		if(err && err.error=='conflict'){
+			
+			console.log('[INFO] Asset Already in Database')
+			
+			json[name] = hash
+			fs.writeFileSync(file, JSON.stringify(json,null,'\t'))
+			
+			console.log('[INFO] Asset Added to Manifest')
+			
+		} else if(err && err.error != 'conflict') {
+			return Error('Asset Conflict! Use `rude add -f %s` to Overwrite',name)
+		} else {
+		
+			var type = 'text/plain'
+			var opts = {
+				rev: res.rev
+			}
+		
+			db.attachment.insert(id, name, data, type, opts, function(err,ok){
+				if(err) return Error(err)
+			
+				console.log('Asset Added:',name)
+			
+				json[name] = hash
+				fs.writeFileSync(file, JSON.stringify(json,null,'\t'))
+			
+			})
+			
+		}
+		
+	})
+}
+
 program
 .command('add')
 .option('-n, --name <NAME>', 'track asset using NAME', null)
+.option('-f, --force'      , 'force update')
 .description('Track a new asset in the local database')
 .action(function(asset,command){
 	if(asset == 'assets.json') return Error('Cannot Track Asset File')
@@ -71,52 +111,75 @@ program
 	var name   = command.name || strip(asset)
 	var shasum = crypto.createHash('sha1');
 	
-	var file   = fs.readFileSync(asset)
+	var data   = fs.readFileSync(asset)
 	var json
 	try{
-		json   = JSON.parse(fs.readFileSync(program.file))
+		json   = JSON.parse(fs.readFileSync(program.manifest))
 	}catch(e){
 		json   = {}
 	}
-	var hash   = shasum.update(file).digest('hex')
+	var hash   = shasum.update(data).digest('hex')
 	
 	var extn   = Path.extname(asset)
 	var path   = name
 	
-	var attachment = {
-		name : path,
-		body : file
-	}
-	
 	var id = hash
 	
-	db.save(id, {}, function(err,res){
-		if(err) return Error(err)
-		
-		db.saveAttachment(res,attachment,function(err,ok){
-			if(err) return Error(err)
-			console.log('Asset Added:',name)
+	if(command.force){
+		doc = db.get(id, function(err,doc){
+			if(err && err.status_code == 404){
+				doc = {}
+			}else if(err){
+				return Error(err)
+			}
 			
-			json[name] = hash
-			fs.writeFileSync(program.file, JSON.stringify(json,null,'\t'))
-			
+			insert(db,doc,id,name,data,json,hash,program.manifest)
 		})
-		
-	})
+	}else{
+		insert(db,{},id,name,data,json,hash,program.manifest)
+	}
 	
 })
 
 program
 .command('push')
 .usage('REMOTE_URL')
-.description('replicate your local database to URL')
+.description('Replicate your local database to URL')
 .action(function(url,command){
+	
+	if(!command) return Error('Invalid Usage')
+	
 	var db   = connect(program.host,program.port,program.name)
 	var opts = {
-		connection_timeout: 1000
+		connection_timeout: 1000,
+		create_target: true
 	}
+	
 	var res = db.replicate(url,opts,function(err){
 		if(err) return Error('Replication Error:', err.error)
+		
+		console.log('[DONE] Replication Complete')
+	})
+})
+
+program
+.command('pull')
+.usage('REMOTE_URL')
+.description('Pull remote changes into your local database')
+.action(function(url,command){
+	
+	if(!command) return Error('Invalid Usage')
+	
+	var db   = nano('http://' + program.host + ':' + program.port).db
+	var opts = {
+		connection_timeout: 1000,
+		create_target: true
+	}
+	
+	var res = db.replicate(url,program.name,opts,function(err){
+		if(err) return Error('Replication Error:', err.error)
+		
+		console.log('[DONE] Replication Complete')
 	})
 })
 
@@ -138,12 +201,13 @@ program
 	'',
 	'Amazon S3 publish requires the ENV variables AWS_ACCESS_KEY_ID, and AWS_SECRET_ACCESS_KEY'
 ].join('\n  '))
+.description('Publish your assets to a production environment')
 .action(function(url,command){
 	
 	if(!command) return Error('Invalid Usage')
 	
 	var db   = connect(program.host,program.port,program.name)
-	var file = fs.readFileSync(program.file)
+	var file = fs.readFileSync(program.manifest)
 	var json = JSON.parse(file)
 	
 	var cons = url.split('://')
@@ -166,10 +230,11 @@ function pad(string,min){
 
 program
 .command('list')
+.description('List assets in your manifest')
 .action(function(command){
 	
 	var db   = connect(program.host,program.port,program.name)
-	var file = fs.readFileSync(program.file)
+	var file = fs.readFileSync(program.manifest)
 	var json = JSON.parse(file)
 	
 	var rude_ = rude.config()
@@ -184,6 +249,7 @@ program
 
 program
 .command('open')
+.description('Open an asset in your browser')
 .action(function(name,command){
 	
 	if(!command) return Error('Invalid Usage')
@@ -193,6 +259,21 @@ program
 	var exec_command = util.format( 'open "%s"', rude_(name) )
 	
 	cp.exec(exec_command)
+	
+})
+
+program
+.command('rm')
+.description('Remove an asset from your manifest')
+.action(function(name,command){
+	
+	var manifest = JSON.parse( fs.readFileSync(program.manifest) )
+	delete manifest[name]
+	
+	fs.writeFileSync(program.manifest, JSON.stringify(manifest))
+	
+	console.log('[INFO] Asset %s Removed from Manifest',name)
+	console.log('[INFO] Use `rude purge %s` to Permenantly Delete Asset from Database',name)
 	
 })
 
